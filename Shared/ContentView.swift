b68c13a461e5e48a04ca489c3d8d92934923d32f
Gags-21 <gagan.bhirangi123@gmail.com>
2022-09-15 13:14:06 +0530

    implemented basic UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,7 +10,68 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack{
+        // Stack
+        ZStack{
+            
+            // Background
+            Image("background").ignoresSafeArea()
+            
+            // Column
+            VStack{
+                
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                // Row
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Pressed Deal")
+                } label: {
+                    Image("dealbutton")
+                }
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    
+                }
+                
+                Spacer()
+                
+            }
             
         }
         
